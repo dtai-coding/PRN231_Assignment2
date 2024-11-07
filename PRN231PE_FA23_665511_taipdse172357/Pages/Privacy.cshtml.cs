07ed84d45d91b00e355c43331ace7bceda334c8b
Pham Duc Tai <149134916+dtai-coding@users.noreply.github.com>
2024-11-07 16:42:58 +0700

    Add project files.

---
+++ PRN231PE_FA23_665511_taipdse172357/Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PRN231PE_FA23_665511_taipdse172357.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
